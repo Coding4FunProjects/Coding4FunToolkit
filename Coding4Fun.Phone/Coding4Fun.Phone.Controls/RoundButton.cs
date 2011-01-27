b54b4f837a789ba00dedaabd363e994bc8d904e2
crutkas_cp <SND\crutkas_cp>
2011-01-27 02:00:09 -0500

    Removed the need for light + dark theme.  Uses opacity mask now to do theming.
    http://coding4fun.codeplex.com/workitem/6145

--- Coding4Fun.Phone/Coding4Fun.Phone.Controls/RoundButton.cs
+++ Coding4Fun.Phone/Coding4Fun.Phone.Controls/RoundButton.cs
@@ -1,24 +1,52 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace Coding4Fun.Phone.Controls
 {
     public class RoundButton : Button
     {
-        public RoundButton()
-        {
-            DefaultStyleKey = typeof(RoundButton);
+        protected ImageBrush OpacityImageBrush;
+        private const string OpacityImageBrushName = "OpacityImageBrush";
+
+		public RoundButton()
+		{
+			DefaultStyleKey = typeof(RoundButton);
             DataContext = this;
-        }
+		}
 
-        public string ImagePath
+		public override void OnApplyTemplate()
         {
-            get { return (string)GetValue(ImagePathProperty); }
-            set { SetValue(ImagePathProperty, value); }
+            base.OnApplyTemplate();
+
+            
+            OpacityImageBrush = GetTemplateChild(OpacityImageBrushName) as ImageBrush;
+
+			if (OpacityImageBrush != null && ImageSource != null)
+            {
+				OpacityImageBrush.ImageSource = ImageSource;
+            }
         }
 
-        // Using a DependencyProperty as the backing store for ImagePath.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty ImagePathProperty =
-            DependencyProperty.Register("ImagePath", typeof(string), typeof(RoundButton), new PropertyMetadata(null));
+		public ImageSource ImageSource
+        {
+			get { return (ImageSource)GetValue(ImageSourceProperty); }
+            set { SetValue(ImageSourceProperty, value); }
+        }
+
+		// Using a DependencyProperty as the backing store for ImageSource.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ImageSourceProperty =
+			DependencyProperty.Register("ImageSource", typeof(ImageSource), typeof(RoundButton), new PropertyMetadata(null, OnImageSource));
+
+		private static void OnImageSource(DependencyObject o, DependencyPropertyChangedEventArgs e)
+		{
+			var sender = o as RoundButton;
+
+			if (sender == null || sender.OpacityImageBrush == null)
+				return;
+
+			var brush = e.NewValue as ImageBrush;
+			sender.OpacityImageBrush = brush;
+		}
     }
 }
