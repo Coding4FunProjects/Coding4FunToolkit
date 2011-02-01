31beee35391902d4c8590833e51bd5c38c78dc18
crutkas_cp <SND\crutkas_cp>
2011-02-01 14:37:55 -0500

    http://coding4fun.codeplex.com/workitem/6183
    Issue was the I'd set IsIndetermine on Unload.  Moved some stuff around to correct issue.

--- Coding4Fun.Phone/Coding4Fun.Phone.Controls/Converters/VisibilityToBooleanConverter.cs
+++ Coding4Fun.Phone/Coding4Fun.Phone.Controls/Converters/VisibilityToBooleanConverter.cs
@@ -1,30 +1,22 @@
 ﻿using System;
 using System.Globalization;
-using System.Net;
-using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Ink;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
+
 
 namespace Coding4Fun.Phone.Controls.Converters
 {
     public class VisibilityToBooleanConverter : IValueConverter
     {
-        private readonly BooleanToVisibilityConverter _boolToVis = new BooleanToVisibilityConverter();
+        private static readonly BooleanToVisibilityConverter BoolToVis = new BooleanToVisibilityConverter();
 
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return _boolToVis.ConvertBack(value, targetType, parameter, culture);
+            return BoolToVis.ConvertBack(value, targetType, parameter, culture);
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return _boolToVis.Convert(value, targetType, parameter, culture);
+            return BoolToVis.Convert(value, targetType, parameter, culture);
         }
     }
 }
--- Coding4Fun.Phone/Coding4Fun.Phone.Controls/PerformanceProgressBar.cs
+++ Coding4Fun.Phone/Coding4Fun.Phone.Controls/PerformanceProgressBar.cs
@@ -1,6 +1,6 @@
-﻿
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
+
 using Coding4Fun.Phone.Controls.Converters;
 
 namespace Coding4Fun.Phone.Controls
@@ -9,24 +9,33 @@ namespace Coding4Fun.Phone.Controls
     /// this is a slightly modified version of Jeff Wilcox
     /// http://www.jeff.wilcox.name/performanceprogressbar/
     /// Performance Progress Bar
+	/// 
+	/// You only need to set Visibility, 
+	/// IsIndeterminate will automatically turn off if Visibilty.Collapsed.
     /// </summary>
     public class PerformanceProgressBar : ProgressBar
     {
         public PerformanceProgressBar()
         {
             DefaultStyleKey = typeof (PerformanceProgressBar);
-            IsIndeterminate = true;
             Unloaded += PerformanceProgressBar_Unloaded;
-
-            var binding = new System.Windows.Data.Binding
-                              {
-                                  Source = this,
-                                  Path = new PropertyPath("Visibility"),
-                                  Converter = new VisibilityToBooleanConverter()
-                              };
-
-            SetBinding(IsIndeterminateProperty, binding);
+			Loaded += PerformanceProgressBar_Loaded;
         }
+
+		void PerformanceProgressBar_Loaded(object sender, RoutedEventArgs e)
+		{
+			var visToBool = new VisibilityToBooleanConverter();
+			IsIndeterminate = (bool) visToBool.Convert(Visibility, null,null, null);
+
+			var binding = new System.Windows.Data.Binding
+			{
+				Source = this,
+				Path = new PropertyPath("Visibility"),
+				Converter = visToBool
+			};
+
+			SetBinding(IsIndeterminateProperty, binding);
+		}
 
         void PerformanceProgressBar_Unloaded(object sender, RoutedEventArgs e)
         {
