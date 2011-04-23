8da21a4cc0f84c9f596a51dfab8cbc1f0490683e
crutkas_cp <SND\crutkas_cp>
2011-04-23 16:54:54 -0400

    refactored to make InputPrompt be the base now for PasswordInputPrompt since they now share a core base

--- Coding4Fun.Phone/Coding4Fun.Phone.Controls/PasswordInputPrompt.cs
+++ Coding4Fun.Phone/Coding4Fun.Phone.Controls/PasswordInputPrompt.cs
@@ -1,16 +1,11 @@
-﻿using System.Diagnostics;
-using System.Text;
-using System.Threading;
+﻿using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Input;
 
 namespace Coding4Fun.Phone.Controls
 {
-    public class PasswordInputPrompt : UserPrompt
+    public class PasswordInputPrompt : InputPrompt
     {
-        protected TextBox InputBox;
-        private const string InputBoxName = "inputBox";
         readonly StringBuilder _inputText = new StringBuilder();
 
         public PasswordInputPrompt()
@@ -21,9 +16,7 @@ namespace Coding4Fun.Phone.Controls
         public override void OnApplyTemplate()
         {
             base.OnApplyTemplate();
-
-            InputBox = GetTemplateChild(InputBoxName) as TextBox;
-
+            
             if (InputBox != null)
             {
                 // manually adding
@@ -31,20 +24,8 @@ namespace Coding4Fun.Phone.Controls
                 // so TextBoxBinding's code doesn't fire
 
                 InputBox.TextChanged += InputBox_TextChanged;
-
-                var valueBinding = new System.Windows.Data.Binding
-                {
-                    Source = InputBox,
-                    Path = new PropertyPath("Text"),
-                };
-
-                SetBinding(ValueProperty, valueBinding);
-                
-                ThreadPool.QueueUserWorkItem(DelayInputSelect);
             }
         }
-
-        
 
         void InputBox_TextChanged(object sender, TextChangedEventArgs e)
         {
@@ -85,45 +66,8 @@ namespace Coding4Fun.Phone.Controls
                 }
                 InputBox.SelectionStart = selectionStart;
             }
-
-            Debug.WriteLine(_inputText.ToString());
         }
-
-        private void DelayInputSelect(object value)
-        {
-            Thread.Sleep(250);
-            Dispatcher.BeginInvoke(() =>
-            {
-                InputBox.Focus();
-                InputBox.SelectAll();
-            });
-        }
-
-        #region public InputScope InputScope
-        /// <summary>
-        /// Gets or sets the
-        /// <see cref="T:System.Windows.Input.InputScope"/>
-        /// used by the Text template part.
-        /// </summary>
-        public InputScope InputScope
-        {
-            get { return (InputScope)GetValue(InputScopeProperty); }
-            set { SetValue(InputScopeProperty, value); }
-        }
-
-        /// <summary>
-        /// Identifies the
-        /// <see cref="P:Microsoft.Phone.Controls.AutoCompleteBox.InputScope"/>
-        /// dependency property.
-        /// </summary>
-        public static readonly DependencyProperty InputScopeProperty =
-            DependencyProperty.Register(
-                "InputScope",
-                typeof(InputScope),
-                typeof(PasswordInputPrompt),
-                null);
-
-
+        
         public char PasswordChar
         {
             get { return (char)GetValue(PasswordCharProperty); }
@@ -133,8 +77,5 @@ namespace Coding4Fun.Phone.Controls
         // Using a DependencyProperty as the backing store for PasswordChar.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty PasswordCharProperty =
             DependencyProperty.Register("PasswordChar", typeof(char), typeof(PasswordInputPrompt), new PropertyMetadata('●'));
-
-
-        #endregion public InputScope InputScope
     }
 }
