b6a52db9915b02b83469df066bfca610dfa34745
stephe_cp <SND\stephe_cp>
2011-05-27 15:23:15 -0400

    Fix for Blend : change the use of ValueChanged for TimeSpanPicker

---
+++ Coding4Fun.Phone/Coding4Fun.Phone.Controls.Toolkit/DateTimePickers/TimeSpanValueChangedEventArgs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace Coding4Fun.Phone.Controls.Toolkit
+{
+    public class TimeSpanValueChangedEventArgs : ValueChangedEventArgs<TimeSpan>
+    {
+        /// <summary>
+        /// Initializes a new instance of the DateTimeValueChangedEventArgs class.
+        /// </summary>
+        /// <param name="oldValue">Old value.</param>
+        /// <param name="newValue">New value.</param>
+        public TimeSpanValueChangedEventArgs(TimeSpan? oldTimeSpanValue, TimeSpan? newTimeSpanValue) 
+            : base(oldTimeSpanValue, newTimeSpanValue)
+        {
+        }
+    }
+}
