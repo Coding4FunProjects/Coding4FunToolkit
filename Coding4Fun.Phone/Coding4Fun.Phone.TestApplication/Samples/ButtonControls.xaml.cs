2397ab75897ac93557075d1fbc7e2f24884ac9c5
crutkas_cp <SND\crutkas_cp>
2011-12-11 04:48:43 -0500

    redid buttons

--- Coding4Fun.Phone/Coding4Fun.Phone.TestApplication/Samples/ButtonControls.xaml.cs
+++ Coding4Fun.Phone/Coding4Fun.Phone.TestApplication/Samples/ButtonControls.xaml.cs
@@ -1,14 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
+using System.Windows.Media.Imaging;
+
+using Coding4Fun.Phone.Controls;
+using Coding4Fun.Phone.TestApplication.Samples.ButtonSamples;
+
 using Microsoft.Phone.Controls;
 
 namespace Coding4Fun.Phone.TestApplication.Samples
@@ -18,26 +16,55 @@ namespace Coding4Fun.Phone.TestApplication.Samples
         public ButtonControls()
         {
             InitializeComponent();
+
+			DataContext = this;
         }
 
-        private void RoundButtons_Click(object sender, RoutedEventArgs e)
-        {
-            NavigateTo("/Samples/ButtonSamples/RoundButtons.xaml");
-        }
+		static readonly ImageSource CheckIcon = new BitmapImage(new Uri("/Media/icons/appbar.check.rest.png", UriKind.RelativeOrAbsolute));
+		static readonly ImageSource RepeatIcon = new BitmapImage(new Uri("/Media/icons/appbar.repeat.png", UriKind.RelativeOrAbsolute));
 
-        private void RoundToggleButtons_Click(object sender, RoutedEventArgs e)
-        {
-            NavigateTo("/Samples/ButtonSamples/RoundToggleButtons.xaml");
-        }
+		public ImageSource RoundButtonImage
+		{
+			get { return (ImageSource)GetValue(RoundButtonImageProperty); }
+			set { SetValue(RoundButtonImageProperty, value); }
+		}
 
-        private void Tiles_Click(object sender, RoutedEventArgs e)
-        {
-            NavigateTo("/Samples/ButtonSamples/Tiles.xaml");
-        }
+		// Using a DependencyProperty as the backing store for RoundButtonImage.  This enables animation, styling, binding, etc...
+		public static readonly DependencyProperty RoundButtonImageProperty =
+			DependencyProperty.Register("RoundButtonImage", typeof(ImageSource), typeof(ButtonControls), new PropertyMetadata(RepeatIcon));
 
-        private void NavigateTo(string page)
+		private void RoundButtonBasicClick(object sender, RoutedEventArgs e)
+		{
+			MessageBox.Show("Ding!");
+		}
+
+		private void ToggleRoundButtonImageClick(object sender, RoutedEventArgs e)
+		{
+			RoundButtonImage = (RoundButtonImage != CheckIcon) ? CheckIcon : RepeatIcon;
+		}
+
+		private void TileClick(object sender, RoutedEventArgs e)
+		{
+			MessageBox.Show("You clicked the tile!");
+		}
+
+		private void PivotSelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			var item = ButtonPivot.SelectedItem as PivotItem;
+
+			if (item != null && (string) item.Tag == "tile")
+				trexStoryboard.Begin();
+			else
+				trexStoryboard.Stop();
+
+		}
+
+		bool _isRed;
+		private void ToggleBackgroundClick(object sender, RoutedEventArgs e)
         {
-            NavigationService.Navigate(new Uri(page, UriKind.Relative));
+			_isRed = !_isRed;
+
+			LayoutRoot.Background = new SolidColorBrush(_isRed ? Colors.Red : Colors.Transparent);
         }
     }
 }
