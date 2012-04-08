e739348a6ae512e56c8816d07e66eb955a33ecf7
crutkas_cp <SND\crutkas_cp>
2012-04-08 01:04:25 -0400

    This seems to get intellisense to work again

--- Coding4Fun.Phone/Coding4Fun.Phone.Controls/ChatBubbleDirection.cs
+++ Coding4Fun.Phone/Coding4Fun.Phone.Controls/ChatBubbleDirection.cs
@@ -6,9 +6,21 @@
 		//Down,
 		//Left,
 		//Right,
+		/// <summary>
+		/// Chat bubble appears on the top, right side
+		/// </summary>
 		UpperRight,
+		/// <summary>
+		/// Chat bubble appears on the top, left side
+		/// </summary>
 		UpperLeft,
+		/// <summary>
+		/// Chat bubble appears on the bottom, right side
+		/// </summary>
 		LowerRight,
+		/// <summary>
+		/// Chat bubble appears on the bottom, left side
+		/// </summary>
 		LowerLeft
 	}
 }
