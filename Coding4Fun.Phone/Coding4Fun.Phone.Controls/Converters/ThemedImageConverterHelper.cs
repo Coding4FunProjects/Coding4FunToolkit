3f1c7a44a36f39b8c17003d9d30fa1127868575d
crutkas_cp <SND\crutkas_cp>
2012-07-15 17:05:13 -0400

    ***BREAKING CHANGE FOR BEHAVIOR****
    http://coding4fun.codeplex.com/workitem/8108
    
    Breaking change but now allows for data driven applications.  Still need to create test cases in the tester app.
    
    Created shared cache for images as well

---
+++ Coding4Fun.Phone/Coding4Fun.Phone.Controls/Converters/ThemedImageConverterHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace Coding4Fun.Phone.Controls.Converters
+{
+	public static class ThemedImageConverterHelper
+	{
+		private static readonly Dictionary<string, BitmapImage> ImageCache = new Dictionary<string, BitmapImage>();
+
+		public static BitmapImage GetImage(string path, bool negateResult = false)
+		{
+			var isDarkTheme = (Application.Current.Resources.Contains("PhoneDarkThemeVisibility") &&
+			                   ((Visibility) Application.Current.Resources["PhoneDarkThemeVisibility"]) == Visibility.Visible);
+
+			if (negateResult)
+				isDarkTheme = !isDarkTheme;
+
+			BitmapImage result;
+			path = string.Format(path, isDarkTheme ? "dark" : "light");
+
+			// Check if we already cached the image
+			if (!ImageCache.TryGetValue(path, out result))
+			{
+				result = new BitmapImage(new Uri(path, UriKind.Relative));
+				ImageCache.Add(path, result);
+			}
+
+			return result;
+		}
+	}
+}
