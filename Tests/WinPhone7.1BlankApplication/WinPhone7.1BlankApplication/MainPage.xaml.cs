57cf8bfbc8517619a45ed2017d0c1749055f0ac1
crutkas_cp <SND\crutkas_cp>
2013-02-12 00:25:51 -0500

    blank tests to shove the nuget packages against.

---
+++ Tests/WinPhone7.1BlankApplication/WinPhone7.1BlankApplication/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Phone.Controls;
+
+namespace WinPhone7._1BlankApplication
+{
+	public partial class MainPage : PhoneApplicationPage
+	{
+		// Constructor
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ Tests/WinPhone8.0BlankApplication/WinPhone8.0BlankApplication/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using WinPhone8._0BlankApplication.Resources;
+
+namespace WinPhone8._0BlankApplication
+{
+	/// <summary>
+	/// Provides access to string resources.
+	/// </summary>
+	public class LocalizedStrings
+	{
+		private static AppResources _localizedResources = new AppResources();
+
+		public AppResources LocalizedResources { get { return _localizedResources; } }
+	}
+}
