f93100055949444b666b796c1a4ea73f32172f1f
crutkas_cp <SND\crutkas_cp>
2013-02-12 03:56:43 -0500

    Attempt to repo http://coding4fun.codeplex.com/workitem/8553 blindly with no success.  Memory jumps and falls back into line.

---
+++ Bugs/ImageTileMemoryIssue/ImageTileMemoryIssue/MainPage.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using Coding4Fun.Toolkit.Controls;
+using Microsoft.Phone.Controls;
+
+namespace ImageTileMemoryIssue
+{
+	public partial class MainPage : PhoneApplicationPage
+	{
+		private string[] flickrImages = new[]
+			{
+				"http://farm8.staticflickr.com/7210/6948063601_9c06c977b7_k_d.jpg",
+				"http://farm3.staticflickr.com/2762/4445148236_16ef33835e_b_d.jpg",
+				"http://farm5.staticflickr.com/4012/4445153474_6ec68c3692_b_d.jpg",
+				"http://farm5.staticflickr.com/4020/4445152626_6bf11c6bcb_b_d.jpg",
+				"http://farm5.staticflickr.com/4050/4444381435_7a25970e82_b_d.jpg",
+				"http://farm5.staticflickr.com/4072/4444380917_257105dcd7_b_d.jpg",
+				"http://farm5.staticflickr.com/4031/4444379947_66fd848c75_b_d.jpg",
+				"http://farm5.staticflickr.com/4058/4445150306_0f0ab6e920_b_d.jpg",
+				"http://farm5.staticflickr.com/4046/4444378913_1cdf61b016_b_d.jpg",
+				"http://farm5.staticflickr.com/4058/4445148506_3f0d18bafe_b_d.jpg",
+				"http://farm5.staticflickr.com/4069/4444378479_45fbb0e488_b_d.jpg",
+				"http://farm3.staticflickr.com/2797/4445148868_3b3a00f0b4_b_d.jpg",
+
+			};
+
+		// Constructor
+		public MainPage()
+		{
+			InitializeComponent();
+
+			Loaded += MainPage_Loaded;
+			
+		}
+
+		void MainPage_Loaded(object sender, RoutedEventArgs e)
+		{
+			CreateAndItemSource();
+		}
+
+		private void CreateAndItemSource()
+		{
+			var imgTile = new ImageTile();
+			imgTile.Click += imgTile_Click;
+			ContentPanel.Children.Add(imgTile);
+
+			var items = new List<Uri>();
+
+			for (int i = 0; i < flickrImages.Count(); i++)
+			{
+				items.Add(new Uri(flickrImages[i], UriKind.RelativeOrAbsolute));
+			}
+
+			imgTile.ItemsSource = items;
+		}
+
+		private void imgTile_Click(object sender, RoutedEventArgs e)
+		{
+			var imgTile = sender as ImageTile;
+
+			if (imgTile != null)
+			{
+				ContentPanel.Children.Remove(imgTile);
+			}
+
+			CreateAndItemSource();
+		}
+	}
+}
