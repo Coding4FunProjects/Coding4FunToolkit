e182fdbdba52737ccfd352267be6e53518bbb144
crutkas_cp <SND\crutkas_cp>
2013-03-11 14:22:22 -0400

    for some reason old name in test would cause a compile issue on a clean solution, renamed seemed to fix it.

--- source/Coding4Fun.Toolkit.Test.WindowsStore/Samples/ChatBubbles/ChatBubbleTextBoxTests.xaml.cs
+++ source/Coding4Fun.Toolkit.Test.WindowsStore/Samples/ChatBubbles/ChatBubbleTextBoxTests.xaml.cs
@@ -14,7 +14,7 @@ namespace Coding4Fun.Toolkit.Test.WindowsStore.Samples.ChatBubbles
 
 		private void AddTextClicked(object sender, RoutedEventArgs e)
 		{
-			DynamicTextHintTest.Text += "c4f ";
+			_dynamicTextHintTest.Text += "c4f ";
 		}
 	}
 }
