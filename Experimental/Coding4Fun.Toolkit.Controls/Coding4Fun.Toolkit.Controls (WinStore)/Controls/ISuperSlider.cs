b9b3acf9b25a5fcd45a998712f29a3f99fde2245
crutkas_cp <SND\crutkas_cp>
2013-05-15 16:08:30 -0400

    slider tests

---
+++ Experimental/Coding4Fun.Toolkit.Controls/Coding4Fun.Toolkit.Controls (WinStore)/Controls/ISuperSlider.cs
@@ -0,0 +1,25 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Coding4Fun.Toolkit.Controls
+{
+	internal interface ISuperSlider
+	{
+		Orientation Orientation { get; set; }
+
+		double StepFrequency { get; set; }
+		double Minimum { get; set; }
+		double Maximum { get; set; }
+		double Value { get; set; }
+
+		double TrackSize { get; set; }
+		double FillSize { get; set; }
+
+		string Title { get; set; }
+
+		Thickness HorizontalPadding { get; set; }
+		Thickness VerticalPadding { get; set; }
+
+		double DisableTrackOpacity { get; set; }
+	}
+}
