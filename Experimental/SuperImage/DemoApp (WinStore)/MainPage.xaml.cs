4f029812fafcbb00f83572d620d4435975d41068
hermitd_cp <SND\hermitd_cp>
2013-05-28 01:11:37 -0400

    Added codebehind usage for content and local image file with super image

--- Experimental/SuperImage/DemoApp (WinStore)/MainPage.xaml.cs
+++ Experimental/SuperImage/DemoApp (WinStore)/MainPage.xaml.cs
@@ -1,15 +1,19 @@
-﻿using System;
+﻿using Coding4Fun.Toolkit.Controls;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using Windows.ApplicationModel;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
@@ -65,8 +69,29 @@ namespace DemoApp__WinStore_
         /// </summary>
         /// <param name="e">Event data that describes how this page was reached.  The Parameter
         /// property is typically used to configure the page.</param>
-        protected override void OnNavigatedTo(NavigationEventArgs e)
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
         {
+            SuperImage sp1 = new SuperImage();
+            BitmapImage bi1 = new BitmapImage() { UriSource = new Uri("ms-appx:/Assets/Images/13.jpg") };
+
+            sp1.Source = bi1;
+
+            this.spCodeBehind.Children.Add(sp1);
+
+            // ensure 14.jpg is in local storage and then use that
+            string contentFolderName = Path.Combine(Package.Current.InstalledLocation.Path, "Assets", "Images");
+            string filename = "14.jpg";
+
+            StorageFolder contentFolder = await StorageFolder.GetFolderFromPathAsync(contentFolderName);
+            StorageFile contentFile = await contentFolder.GetFileAsync(filename);
+
+            await contentFile.CopyAsync(ApplicationData.Current.LocalFolder);
+
+            SuperImage sp2 = new SuperImage();
+            BitmapImage bi2 = new BitmapImage() { UriSource = new Uri("ms-appdata:///local/14.jpg") };
+            sp2.Source = bi2;
+
+            this.spCodeBehind.Children.Add(sp2);
         }
     }
 }
