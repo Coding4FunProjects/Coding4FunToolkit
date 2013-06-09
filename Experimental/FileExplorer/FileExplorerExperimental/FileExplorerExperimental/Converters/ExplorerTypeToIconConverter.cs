1584fcda7823d540c1708e94e739132aa13ecb07
DennisDelimarsky_cp <SND\DennisDelimarsky_cp>
2013-06-09 19:44:06 -0400

    FileExplorer experimental solution.

---
+++ Experimental/FileExplorer/FileExplorerExperimental/FileExplorerExperimental/Converters/ExplorerTypeToIconConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace FileExplorerExperimental.Converters
+{
+    public class ExplorerTypeToIconConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            bool isFolder = (bool)value;
+
+            if (isFolder)
+                return new Uri("/Assets/Icons/folder.png", UriKind.Relative);
+            else
+                return new Uri("/Assets/Icons/file.png", UriKind.Relative);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Experimental/FileExplorer/FileExplorerExperimental/FileExplorerExperimental/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using FileExplorerExperimental.Resources;
+
+namespace FileExplorerExperimental
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ExplorerControl.Show();
+        }
+    }
+}
